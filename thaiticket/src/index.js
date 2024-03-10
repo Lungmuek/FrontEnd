6abe439097b5ee29a8e5560149b963920f551ffd
muyumq <aphiwitsomphet@gmail.com>
2024-03-10 14:46:53 +0700

    router-dom

--- thaiticket/src/index.js
+++ thaiticket/src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HeadEvent from './components/HeadEvent';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/event",
+    element: <HeadEvent/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
